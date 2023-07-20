302fbab77aa4ecf1cf38d9e7cec5c56b3b99f707
Darshan Vaniya <darshan.vaniya@aprilinnovations.com>
2023-07-20 18:55:27 +0530

    initial setup

---
+++ src/api/video-link/routes/video-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-link router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::video-link.video-link');
---
+++ src/api/video-link/services/video-link.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-link service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::video-link.video-link');
